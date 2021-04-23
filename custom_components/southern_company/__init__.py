a202495931fdc996996af0333f5023c2574e57ff
Ryan Johnson <43426700+ryanjohnsontv@users.noreply.github.com>
2021-04-23 13:10:23 -0500

    Update __init__.py

--- custom_components/southern_company/__init__.py
+++ custom_components/southern_company/__init__.py
@@ -1,1 +1,58 @@
+"""Support for Southern Company sensors."""
+import datetime
+import logging
 
+import voluptuous as vol
+
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.config_entries import SOURCE_IMPORT
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
+import homeassistant.helpers.config_validation as cv
+
+from .const import (
+    CONF_ACCOUNT,
+    DOMAIN,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+MIN_TIME_BETWEEN_UPDATES = datetime.timedelta(seconds=600)
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_USERNAME): cv.string,
+        vol.Required(CONF_PASSWORD): cv.string,
+        vol.Required(CONF_ACCOUNT): cv.string,
+    }
+)
+
+
+async def async_setup(hass, config):
+    """Set up the Southern Company component."""
+    hass.data[DOMAIN] = {}
+
+    if not hass.config_entries.async_entries(DOMAIN) and DOMAIN in config:
+        hass.async_create_task(
+            hass.config_entries.flow.async_init(
+                DOMAIN, context={"source": SOURCE_IMPORT}, data=config[DOMAIN]
+            )
+        )
+
+    return True
+
+
+async def async_setup_entry(hass, config_entry):
+    """Set up Southern Company as config entry."""
+    if DOMAIN not in hass.data:
+        hass.data[DOMAIN] = {}
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+    return True
+
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_unload(config_entry, "sensor")
+    return True
